54e740058278ec73335231a7202cadcf34b8e8e9
abbeake <abbe.akeson1999@gmail.com>
2025-01-01 16:22:24 +0100

    routerlinks till andra sidorna

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { Member1Component } from './member1/member1.component';
+import { Member2Component } from './member2/member2.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // Default route (home page)
     { path: 'about', component: AboutComponent }, // Route for the About page
+    { path: 'member1', component: Member1Component },
+    { path: 'member2', component: Member2Component },
+    { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: '' }, // Redirect unknown routes to home
   ];
