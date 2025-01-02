3c531273b0b34688c76e9f06b48d641c19a4e1c7
abbeake <abbe.akeson1999@gmail.com>
2025-01-02 19:45:12 +0100

    routers

--- src/app/member1/member1.component.ts
+++ src/app/member1/member1.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-member1',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member1.component.html',
   styleUrl: './member1.component.css'
 })
--- src/app/member2/member2.component.ts
+++ src/app/member2/member2.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-member2',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member2.component.html',
   styleUrl: './member2.component.css'
 })
