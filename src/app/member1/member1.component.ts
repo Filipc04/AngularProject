7821524a7059b49b47e51a7903913fb69bf9a3bc
Filip C <bengtfille2@gmail.com>
2025-01-05 17:46:09 +0100

    Accordion succesfully added, styling needed

--- src/app/member1/member1.component.ts
+++ src/app/member1/member1.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import this for *ngFor
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap'; // Import this for accordion
 
 @Component({
   selector: 'app-member1',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, CommonModule, NgbAccordionModule], // Add CommonModule and NgbAccordionModule here
   templateUrl: './member1.component.html',
-  styleUrl: './member1.component.css'
+  styleUrls: ['./member1.component.css']
 })
 export class Member1Component {
-
+  items = ['First', 'Second', 'Third'];
 }
