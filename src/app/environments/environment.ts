bf9ee8c2c2915dbc80f06fbcd81c642227c65627
abbeake <abbe.akeson1999@gmail.com>
2025-01-06 13:44:46 +0200

    Made navbar pop out and connected to firebase

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCBkBzxajrywoc5Uxc6ToQxKWGw4bm5p3c",
+  authDomain: "angularanime.firebaseapp.com",
+  projectId: "angularanime",
+  storageBucket: "angularanime.firebasestorage.app",
+  messagingSenderId: "473892076915",
+  appId: "1:473892076915:web:e5683e8c82ed4f529cd906",
+  measurementId: "G-9BYHN3QQS2"
+};
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isNavbarVisible: boolean = false;
 
+  toggleNavbar(): void {
+    this.isNavbarVisible = !this.isNavbarVisible;
+  }
 }
