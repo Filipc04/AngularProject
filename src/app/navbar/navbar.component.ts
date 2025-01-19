36b75b3a29bfe7ec38547b822da92fbda845ab51
Filip C <bengtfille2@gmail.com>
2025-01-19 13:06:29 +0100

    alert added to navbar as well

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../auth.service'; 
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterLink, RouterLinkActive, RouterOutlet, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   isNavbarVisible: boolean = false;
 
-  constructor(public authService: AuthService) {}
+  @ViewChild('toast', { static: false }) toast!: ElementRef;
+
+  constructor(public authService: AuthService, private renderer: Renderer2) {}
 
   toggleNavbar(): void {
     this.isNavbarVisible = !this.isNavbarVisible;
+  }
+
+  checkAuthAndShowToast(event: Event): void {
+    if (!this.authService.currentUserSig()) {
+      event.preventDefault(); // Prevent navigation
+      this.showToast();
+    }
+  }
+
+  showToast() {
+    this.renderer.addClass(this.toast.nativeElement, 'show');
+    setTimeout(() => {
+      this.renderer.removeClass(this.toast.nativeElement, 'show');
+    }, 3000);
   }
 }
